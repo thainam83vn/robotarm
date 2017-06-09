65c4984c7e57205f417bb50f113875ab301ecb34
thai <thainam83vn@gmail.com>
2017-06-08 21:30:00 -0400

    'u'

--- control-ui.py
+++ control-ui.py
@@ -3,6 +3,21 @@ import SocketServer
 
 PORT = 8000
 
+
+# This class will handles any incoming request from
+# the browser
+class myHandler(BaseHTTPRequestHandler):
+    # Handler for the GET requests
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        else:
+            parts = self.path.split('/')
+            servoid = parts[len(parts) - 2]
+            degree = parts[len(parts) - 1]
+            print(servoid, ' ', degree)
+
+
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
