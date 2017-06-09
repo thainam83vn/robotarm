e8004233db6e8dd8abe52f5263abfbf86ee8c9c9
thai <thainam83vn@gmail.com>
2017-06-08 21:36:54 -0400

    'u'

--- control-ui.py
+++ control-ui.py
@@ -11,8 +11,41 @@ class myHandler(BaseHTTPRequestHandler):
     # Handler for the GET requests
     def do_GET(self):
         if self.path == "/":
-            self.path = "index.html"
+            self.path = "/index.html"
             print('index.html')
+            try:
+                # Check the file extension required and
+                # set the right mime type
+
+                sendReply = False
+                if self.path.endswith(".html"):
+                    mimetype = 'text/html'
+                    sendReply = True
+                if self.path.endswith(".jpg"):
+                    mimetype = 'image/jpg'
+                    sendReply = True
+                if self.path.endswith(".gif"):
+                    mimetype = 'image/gif'
+                    sendReply = True
+                if self.path.endswith(".js"):
+                    mimetype = 'application/javascript'
+                    sendReply = True
+                if self.path.endswith(".css"):
+                    mimetype = 'text/css'
+                    sendReply = True
+
+                if sendReply == True:
+                    # Open the static file requested and send it
+                    f = open(curdir + sep + self.path)
+                    self.send_response(200)
+                    self.send_header('Content-type', mimetype)
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                    f.close()
+                return
+
+            except IOError:
+                self.send_error(404, 'File Not Found: %s' % self.path)
         else:
             parts = self.path.split('/')
             servoid = parts[len(parts) - 2]
